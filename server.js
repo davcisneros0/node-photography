dce3ef1decb60454e510951d3e2176f5b92bfc55
David <Davcisneros2000@gmail.com>
2024-11-17 14:27:59 -0500

    Add routes for each webpage

--- server.js
+++ server.js
@@ -4,5 +4,21 @@ const path = require('path');
 //CREATE SERVER
 const app = express();
 
+//DISPLAY STATIC FILES FROM PUBLIC DIRECTORY
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true}));
+
+//CREATE ROUTES
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
 app.listen(3002);
